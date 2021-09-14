c4c8ffa6d11abf13c9bd6b348769d142cd053863
noelly-nay <noelankya096@gmail.com>
2021-09-14 13:34:39 +0300

    ...

--- routes/authors.js
+++ routes/authors.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const author = require('../models/author')
 const router = express.Router()
 const Author = require('../models/author')
 
@@ -9,12 +10,26 @@ router.get('/', (req,res) => {
 
 //New Author Route
 router.get('/new', (req,res) => {
-    res.render('authors/new',{ author: new Author()})
+    res.render('authors/new',{ author: new Author() })
 })
 
 //Create Author Route
-router.get('/', (req,res) => {
-    res.send('Create')
+router.post('/', (req,res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if(err) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else {
+            //res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
 })
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Router = require("./routes")
 
@@ -19,6 +20,10 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ 
+  limit: '10mb',
+  extended: 'false'
+}))
 
 try {
     mongoose.connect(process.env.DATABASE_URL, {
